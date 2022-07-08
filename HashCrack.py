a280af273b2c9c5703886ce54c4c4b700e748130
LoganGibler <Logan.Gibler@gmail.com>
2022-07-07 20:23:28 -0400

    better error messages

--- HashCrack.py
+++ HashCrack.py
@@ -5,16 +5,18 @@ passHash = input("Enter password hash:")
 wordList = open("LeakedPasswords.txt","r")
 
 def hashCrack(passHash):
+    count = 0
     for word in wordList:
         word = word.strip()
-        print(word,"word in for loop")
+        # print(word,"word in for loop")
         guess = md5(word.encode("utf-8"))
-        print(guess.hexdigest(),"digest")
+        # print(guess.hexdigest(),"digest")
 
         if guess.hexdigest() == passHash:
             print("Password Found")
             print("Password is " + word)
-            break
-        else:
-            print("password not found")
+            count += 1
+        
+    if count == 0:
+        print("Password not found")
 hashCrack(passHash)
