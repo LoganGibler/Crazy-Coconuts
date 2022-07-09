a921c7a04fe5494fa29c4beeb2fd9c67048ea98e
LoganGibler <Logan.Gibler@gmail.com>
2022-07-08 20:21:22 -0400

    added password grader

--- PasswordTool.py
+++ PasswordTool.py
@@ -40,12 +40,12 @@ def generator():
     return password
     # passcheckgenerator(password)
 
-special_characters = """!@#$%^&*()-+?_=,<>/"""
+
 
 def passcheckgenerator(password):
     uppercount = 0
     lowercount = 0
-    print("Checked Password:  ", password)
+
     for letter in password:
         if letter in special_characters:
             continue
@@ -69,35 +69,56 @@ def passcheckgenerator(password):
     else:
         print("Needs at least 1-2 special characters")
 
+special_characters = """!@#$%^&*()-+?_=,<>/"""
 
 def passcheck():
     password = input("Enter password to check: ")
     uppercount = 0
     lowercount = 0
-    print("Checked Password:  ", password)
+    specialcharcount = 0
+    numbercount = 0
+    length_grade = 0
+
     for letter in password:
         if letter in special_characters:
             continue
         if letter == letter.upper():
-            uppercount += 1
+            uppercount = 1
         if letter == letter.lower():
-            lowercount += 1
+            lowercount = 1
+        if letter in numberlist:
+            numbercount = 1
+    if len(password) >= 8:
+        length_grade = 1
+    if len(password) >= 12:
+        length_grade += 1
     if uppercount == 0:
-            print("Password needs an uppercase letter")
+            print("Password needs an uppercase letter :(")
     if lowercount == 0:
-            print("Password needs a lowercase letter")
+            print("Password needs a lowercase letter :(")
     
     if len(password) < 8:
-        print("Ensure password is at least 8 characters long")
+        print("Ensure password is at least 8 characters long :(")
     else:
-        print("Password has sufficient length ") #more specific, ie 8-11 characters weak, 12-14 moderate, 15+ strong
+        print("Password has sufficient length :)") #more specific, ie 8-11 characters weak, 12-14 moderate, 15+ strong
 
     
     if any(char in special_characters for char in password):
-        print("Password has special characters")
+        specialcharcount = 1
+        print("Password has special characters :)")
     else:
         print("Needs at least 1-2 special characters")
 
+    pass_grade = length_grade + specialcharcount + numbercount + uppercount + lowercount
+  
+    if pass_grade == 6:
+        print("PTools Password Grade:  Excellent")
+    if pass_grade == 5:
+        print("PTools Password Grade:  Great")
+    if pass_grade == 4:
+        print("PTools Password Grade:  Good")
+    if pass_grade <= 3:
+        print("PTools Password Grade:  Bad")
     
 def compare():
     string1 = input("Enter password to search for: ")
